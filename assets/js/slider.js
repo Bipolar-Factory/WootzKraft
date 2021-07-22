d95fdb39e673e223caa4c97e563b0691dcd5e38f
divyabharathimk29 <72742118+divyabharathimk29@users.noreply.github.com>
2021-07-22 18:24:02 +0530

    added image section changes

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -58,3 +58,36 @@ $(".icon-10").mouseover(function (e) {
     $(this).attr("src", "assets/img/icons/new-icons/10_1.png");
     // $(this).attr("src", $(this).attr("src").replace(".gif", ".png"));
 });
+
+
+// Vidoe Slider
+
+
+
+(function(){
+  // setup your carousels as you normally would using JS
+  // or via data attributes according to the documentation
+  // https://getbootstrap.com/javascript/#carousel
+  $('#carousel123').carousel({ interval: 2000 });
+  $('#carouselABC').carousel({ interval: 3600 });
+}());
+
+(function(){
+  $('.carousel-showmanymoveone .item').each(function(){
+    var itemToClone = $(this);
+
+    for (var i=1;i<4;i++) {
+      itemToClone = itemToClone.next();
+
+      // wrap around if at end of item collection
+      if (!itemToClone.length) {
+        itemToClone = $(this).siblings(':first');
+      }
+
+      // grab item, clone, add marker class, add to collection
+      itemToClone.children(':first-child').clone()
+        .addClass("cloneditem-"+(i))
+        .appendTo($(this));
+    }
+  });
+}());
